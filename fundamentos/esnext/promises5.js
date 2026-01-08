07f516b75ce08c710ecd67c280c210f2ad750738
Natanael <122684584+Natan1801@users.noreply.github.com>
2026-01-08 18:00:27 -0300

    promises 5

---
+++ fundamentos/esnext/promises5.js
@@ -0,0 +1,25 @@
+function funcionarOuNao(valor){
+    return new Promise((resolve, reject) => {
+      try{
+        con.log('temp')
+        if(Math.random() < chanceErro){
+          reject('Ocorreu um erro!')
+       } else{
+          resolve(valor)
+       }
+      } catch(e){
+        reject(e)
+      }
+       
+    })
+}
+
+funcionarOuNao('Testando...', 0.5)
+    .then(v => `Valor: ${v}`)
+    .then(
+      v => console.log(v),
+      err => console.log(`Erro específico: ${err}`)
+    )
+    .then(() => console.log('Quase fim!'))
+    .catch(err => console.log(`Erro: ${err}`))
+    .then(() => console.log('Fim!'))
